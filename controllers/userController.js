3e4a2dab2ac709a6fec739420e1cc27f9ec6bed8
Marcos Mantovani <mantovani.marcos@gmail.com>
2021-09-21 19:14:04 -0300

    added session logout

--- controllers/userController.js
+++ controllers/userController.js
@@ -5,12 +5,14 @@ exports.login = function(req, res) {
   let user = new User(req.body)
   user.login().then(result => {
     req.session.user = {favColor: 'blue', username: user.data.username}
-    res.send(result)
+    req.session.save(() => res.redirect('/'))
   }).catch(e => res.send(e?.errors ?? e)) //if error object has errors property send it else sen just error (defaults to message)
 }
 
-exports.logout = function() {
-
+exports.logout = function(req, res) {
+  req.session.destroy(() => {
+    res.redirect('/')
+  })
 }
 
 exports.register = async function(req, res) {
