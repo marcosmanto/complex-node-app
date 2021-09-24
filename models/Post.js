b4e769d8679dba0aee474fdf73724e1b96eba8d5
Marcos Mantovani <mantovani.marcos@gmail.com>
2021-09-24 00:13:15 -0300

    Post data saving in database and response in json after create

--- models/Post.js
+++ models/Post.js
@@ -1,11 +1,12 @@
 const { cleanObject } = require("../modules/Utils")
-
+const postsCollection = require('../db').db().collection('posts')
 class Post {
-  #transformations = [onlyString, lower]
   #allowedFields = ['title', 'body']
+  #transformations = [onlyString, lower]
 
   constructor(data) {
     this.data = data
+    this.errors = []
   }
 
   cleanUp() {
@@ -26,13 +27,29 @@ class Post {
   }
 
   validate() {
-
+    if(this.data.title === '') { this.errors.push('You must provide a title.')}
+    if(this.data.body === '') { this.errors.push('You must provide post content.')}
   }
 
-  async create() {
-    this.cleanUp()
-    this.data.createDate = new Date()
-    //this.validate()
+  create() {
+    return new Promise((resolve, reject) => {
+      this.cleanUp()
+      this.validate()
+      if(!this.errors.length) {
+        this.data.createDate = new Date()
+        // save post into database
+        postsCollection.insertOne(this.data)
+          .then(() => {
+            resolve()
+          })
+          .catch(() => {
+            this.errors.push('Please try again later.')
+            reject(this.errors)
+          })
+      } else {
+        reject(this.errors)
+      }
+    })
   }
 
 }
