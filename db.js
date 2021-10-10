641cafad3dd868ced30f92745cba657b591a36f5
Marcos Mantovani <mantovani.marcos@gmail.com>
2021-10-10 19:46:32 -0300

    removed dotenv dev dependency

--- db.js
+++ db.js
@@ -1,22 +1,22 @@
 const mongodb = require('mongodb')
+
+/* // remove dotenv for Heroku
 const dotenv = require('dotenv')
-
 dotenv.config()
+*/
 
-mongodb.MongoClient.connect(process.env.CONNECTIONSTRING, {useNewUrlParser: true, useUnifiedTopology: true},
-  (err, client) => {
-    if( err && err.stack.toLowerCase().includes('timeout')) {
-      console.log('Connection timeout. Check if mongodb server is running.')
-      process.exit(0)
-    }
-
-    const db =  client.db()
-
-    if (db) console.log('Connected to database.')
-
-    module.exports = client
-
-    const app = require('./app')
-    app.listen(process.env.PORT)
+mongodb.MongoClient.connect(process.env.CONNECTIONSTRING, { useNewUrlParser: true, useUnifiedTopology: true }, (err, client) => {
+  if (err && err.stack.toLowerCase().includes('timeout')) {
+    console.log('Connection timeout. Check if mongodb server is running.')
+    process.exit(0)
   }
-)
+
+  const db = client.db()
+
+  if (db) console.log('Connected to database.')
+
+  module.exports = client
+
+  const app = require('./app')
+  app.listen(process.env.PORT)
+})
